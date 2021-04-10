80c9b124d97573dd6ada67d52bc4b5691d610104
ArturoSerranoDev <asm.arturo.developer@gmail.com>
2021-04-10 13:25:29 +0200

    MainMenuTransitions

--- Assets/Scripts/Controllers/Singleton/UIMenuController.cs
+++ Assets/Scripts/Controllers/Singleton/UIMenuController.cs
@@ -1,18 +1,81 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using TMPro;
 
 public class UIMenuController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public CanvasGroup StartingPanelCanvasGroup;
+    public GameObject mainMenuPanel;
+    public CanvasGroup mainMenuPanelCanvasGroup;
+    public CanvasGroup levelSelectCanvasGroup;
+    public CanvasGroup artGalleryCanvasGroup;
+
+    public TextMeshProUGUI creditsText;
+    public TextMeshProUGUI creditsMadeByText;
+
+
+    bool tweeningCredits;
+
+    public void GoToMainMenu()
     {
-        
+        StartingPanelCanvasGroup.DOFade(0, 0.25f).From(1).SetEase(Ease.InOutSine).OnComplete(() => StartingPanelCanvasGroup.gameObject.SetActive(false));
+
+        mainMenuPanel.SetActive(true);
+
+        mainMenuPanelCanvasGroup.DOFade(1, 0.25f).From(0).SetDelay(0.25f).SetEase(Ease.InOutSine);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void GoToMainMenuFromLevelSelect()
     {
-        
+        levelSelectCanvasGroup.DOFade(0, 0.25f).From(1).OnComplete(() => levelSelectCanvasGroup.gameObject.SetActive(false)).SetEase(Ease.InOutSine);
+
+        mainMenuPanel.SetActive(true);
+
+        mainMenuPanelCanvasGroup.DOFade(1, 0.25f).From(0).SetDelay(0.25f).SetEase(Ease.InOutSine);
+    }
+
+    public void GoToMainMenuFromArtGallery()
+    {
+        artGalleryCanvasGroup.DOFade(0, 0.25f).From(1).OnComplete(() => artGalleryCanvasGroup.gameObject.SetActive(false)).SetEase(Ease.InOutSine);
+
+        mainMenuPanel.SetActive(true);
+
+        mainMenuPanelCanvasGroup.DOFade(1, 0.25f).From(0).SetDelay(0.25f).SetEase(Ease.InOutSine);
+    }
+
+    public void GoToLevelSelect()
+    {
+        mainMenuPanelCanvasGroup.DOFade(0, 0.25f).From(1).OnComplete(() => mainMenuPanelCanvasGroup.gameObject.SetActive(false)).SetEase(Ease.InOutSine);
+
+        levelSelectCanvasGroup.gameObject.SetActive(true);
+
+        levelSelectCanvasGroup.DOFade(1, 0.25f).From(0).SetDelay(0.25f).SetEase(Ease.InOutSine);
+    }
+
+    public void GoToArtGallery()
+    {
+        mainMenuPanelCanvasGroup.DOFade(0, 0.25f).From(1).OnComplete(() => mainMenuPanelCanvasGroup.gameObject.SetActive(false)).SetEase(Ease.InOutSine);
+
+        artGalleryCanvasGroup.gameObject.SetActive(true);
+
+        artGalleryCanvasGroup.DOFade(1, 0.25f).From(0).SetDelay(0.25f).SetEase(Ease.InOutSine);
+    }
+
+    public void ShowCredits()
+    {
+        if (tweeningCredits)
+            return;
+
+        tweeningCredits = true;
+        creditsText.DOFade(0, 0.25f).From(1);
+
+        creditsMadeByText.DOFade(1, 0.25f).From(0).SetDelay(0.25f);
+
+        creditsMadeByText.DOFade(0, 0.25f).SetDelay(0.25f+1f).OnComplete(() => tweeningCredits = false);
+
+        creditsText.DOFade(1, 0.25f).SetDelay(0.25f +1f+0.5f);
+
     }
 }
